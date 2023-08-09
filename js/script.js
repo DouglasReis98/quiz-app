5e5df7d9eaec60d03ce089281f498b7fa3fadb2a
DouglasReis <douglasamaralreis@outlook.com>
2023-08-08 22:01:02 -0300

    funções_finalizadas

--- js/script.js
+++ js/script.js
@@ -8,10 +8,9 @@ const numQuestao = document.getElementById("num-questao");
 // const alternativas = document.querySelector(".alternativas");
 const anterior = document.getElementById("anterior");
 const seguinte = document.getElementById("seguinte");
-const textoFinal = document.querySelector(".final h4");
 const divQuestoes = document.querySelector(".questoes");
 const divFinal = document.querySelector(".final");
-
+const btnReiniciar = document.getElementById("reiniciar")
 
 let questAtual = 0;
 let respCorretas = 0;
@@ -24,7 +23,7 @@ btnIniciar.addEventListener("click", () => {
 
 
 function carregarQuestao() {
-    divQuestoes.style.display = "block";
+    divQuestoes.style.display = "flex";
     const item = questoes[questAtual];
     divQuestoes.innerHTML = `<h3 class="text-center num-questao">Questão ${questAtual + 1} de 10</h3>
                              <h4 class="questao">${item.questao}</h4>`;
@@ -76,8 +75,22 @@ function proxQuestao() {
     } 
 }
 
+let textoFinal;
+
 function resultado(){
+    textoFinal = document.createElement("h4"); 
+    textoFinal.className = "text-center";
     textoFinal.innerHTML= `Você acertou ${respCorretas} de ${questoes.length}!`
+    divFinal.insertBefore(textoFinal, btnReiniciar);
     divQuestoes.style.display = 'none';
-    divFinal.style.display = "block";
+    divFinal.style.display = "flex";
 }
+
+btnReiniciar.addEventListener("click", () => {
+    textoFinal.remove();
+    divQuestoes.style.display = 'block';
+    divFinal.style.display = "none";
+    questAtual = 0;
+    respCorretas = 0;    
+    carregarQuestao();
+})
